e062d144250263c540c1d8473fc64f5338d405db
boyyveedo <talktoboyveedo@gmail.com>
2025-07-30 19:43:12 +0100

    swagger updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { GroupsModule } from './modules/groups/groups.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -24,5 +26,7 @@ import { GroupsModule } from './modules/groups/groups.module';
     UsersModule,
     GroupsModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
